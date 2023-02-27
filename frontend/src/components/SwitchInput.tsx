18221fc86e4b270532bb3848c98bde283365a9fd
Marcelo <marcelo.guzman.g@usach.cl>
2023-02-27 18:59:43 -0300

    fix switch container padding

--- frontend/src/components/SwitchInput.tsx
+++ frontend/src/components/SwitchInput.tsx
@@ -9,9 +9,11 @@ const SwitchInput = ({ nameId }: SwitchInputProps) => {
   return (<>
     <input id={nameId} name={nameId} checked={isOn} className="hidden" type="checkbox" 
       onChange={e => setIsOn(e.target.checked)} />
-    <div tabIndex={0} onClick={() => setIsOn(prev => !prev)} className="focus:outline-none focus:ring-2 focus:inset-2 focus:ring-violet-500 w-10 bg-gray-300 rounded-full cursor-pointer p-1">
+    <div tabIndex={0} onClick={() => setIsOn(prev => !prev)} 
+      className={`${isOn ? "bg-sky-400" : "bg-gray-300"} box-content focus:outline-none focus:ring-2 focus:inset-2 focus:ring-violet-500 
+       p-0.5 w-8 h-4 rounded-full cursor-pointer flex justify-start items-center`}>
       <div style={{ transform: transitionAmount }}
-        className={`${isOn ? "bg-sky-400" : "bg-white"} shadow transition-all h-4 w-4 rounded-full`}>
+        className={`bg-white shadow transition-all h-4 w-4 rounded-full`}>
       </div>
     </div>
   </>)
